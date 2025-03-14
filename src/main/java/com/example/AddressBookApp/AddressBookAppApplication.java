325ec11d3934399d114d1211b182a1b3f683ff18
Abhishek1743 <abhisheksagar1113@gmail.com>
2025-03-14 21:59:25 +0530

    Added Redis

--- src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
+++ src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
@@ -3,9 +3,11 @@ package com.example.AddressBookApp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableCaching
 @Slf4j
 public class AddressBookAppApplication {
 
